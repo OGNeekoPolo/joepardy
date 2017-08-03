a84fe64d2ff0772b0ed6bedc57c5289ece625b40
Nik Trabue <nktrabue@gmail.com>
2017-08-03 14:54:11 -0400

    Bug fix in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
+
   get 'sessions/new'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
@@ -9,11 +9,9 @@ Rails.application.routes.draw do
 
   resources :users
 
-=======
   resources :quizzes do
     resources :questions do
       resources :answers
     end
   end
->>>>>>> 9cf644edcdfe07aeb4714da9b7bafc2fa2f3b83d
 end
