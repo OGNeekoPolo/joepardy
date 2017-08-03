a5fb9a3d99d57398fd575c8b7b179349d35a2bad
rlporter <rebekkah1@me.com>
2017-08-03 12:39:25 -0400

    add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/show'
+  resources :users
 
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
