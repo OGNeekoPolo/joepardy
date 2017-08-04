635f4e93194c2def985f61d6e879fb871d8d681b
rlporter <rebekkah1@me.com>
2017-08-04 09:55:55 -0400

    update answer controller to add create method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,4 +14,20 @@ class AnswersController < ApplicationController
     else
       @answer = Answer.find(params[:answer_id])
   end
+
+  def create
+    @answer = Answer.create(answer_params)
+
+    if @answer.save
+      redirect_to @quiz.question
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:first_option, :second_option, :third_option, :correct_answer)
+  end
 end
